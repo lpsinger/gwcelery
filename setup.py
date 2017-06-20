616b2b56dc001734f85251cac1999959c9025280
Leo Singer <leo.singer@ligo.org>
2017-06-20 15:26:39 -0400

    Remove ini file

--- setup.py
+++ setup.py
@@ -40,7 +40,4 @@ setup(
         'git+https://github.com/lpsinger/celery-singleton@subclass_apply_async#celery_singleton-0.0.2.1'
     ],
     packages=find_packages(),
-    package_data={
-        '': ['*.ini']
-    }
 )
