101ffa7a5ff8fbd9b50a9663e701d2818f051e0c
Leo Singer <leo.singer@ligo.org>
2017-06-20 12:06:28 -0400

    Clean up and add FITS header

--- gwcelery/tasks.py
+++ gwcelery/tasks.py
@@ -1,9 +1,17 @@
 # Standard library imports
-from tempfile import NamedTemporaryFile
+from __future__ import print_function
+from contextlib import contextmanager
+import io
+import json
+import os
+from tempfile import NamedTemporaryFile as _NamedTemporaryFile
 
 # Third-party imports
+import astropy.io.fits
 from celery import Celery, group
 from celery.utils.log import get_task_logger
+from ligo.gracedb.rest import GraceDb
+import six
 
 
 # Celery application object.
@@ -19,12 +27,31 @@ app = Celery('tasks', backend='redis://', broker='redis://', config_source=dict(
 log = get_task_logger(__name__)
 
 
+@contextmanager
+def NamedTemporaryFile(**kwargs):
+    """Convenience wrapper for NamedTemporaryFile that writes some data to
+    the file before handing it to the calling code."""
+    # Make a copy so that we don't modify kwargs
+    kwargs = dict(kwargs)
+
+    content = kwargs.pop('content', None)
+    if isinstance(content, six.binary_type):
+        kwargs['mode'] = 'w+b'
+    elif isinstance(content, six.text_type):
+        kwargs['mode'] = 'w+'
+    elif content is not None:
+        raise TypeError('content is of unknown type')
+    with _NamedTemporaryFile(**kwargs) as f:
+        if content is not None:
+            f.write(content)
+            f.flush()
+            f.seek(0)
+        yield f
+
+
 @app.task(ignore_result=True)
 def dispatch(payload):
     """Parse an LVAlert message and dispatch it to other tasks."""
-    import json
-    import os
-
     # Parse JSON payload
     alert = json.loads(payload)
 
@@ -52,6 +79,10 @@ def dispatch(payload):
 
 def annotate_fits(versioned_filename, filebase, graceid, service, tags):
     """Perform various annotations on a sky map."""
+    fits_header_message = (
+        'FITS headers for <a href="/apiweb/events/{graceid}/files/'
+        '{versioned_filename}">{versioned_filename}</a>').format(
+            graceid=graceid, versioned_filename=versioned_filename)
     plot_allsky_message = (
         'Mollweide projection of <a href="/apiweb/events/{graceid}/files/'
         '{versioned_filename}">{versioned_filename}</a>').format(
@@ -60,16 +91,15 @@ def annotate_fits(versioned_filename, filebase, graceid, service, tags):
         'Volume rendering of <a href="/apiweb/events/{graceid}/files/'
         '{versioned_filename}">{versioned_filename}</a>').format(
             graceid=graceid, versioned_filename=versioned_filename)
-    download(versioned_filename, graceid, service)
+    content = download(versioned_filename, graceid, service)
+    (fits_header.s(versioned_filename, content) | upload.s(filebase + '.html', graceid, service, fits_header_message, tags)).delay()
     (plot_allsky.s(content) | upload.s(filebase + '.png', graceid, service, plot_allsky_message, tags)).delay()
-    (plot_volume.s(content) | upload.s(filebase + '.volume.png', graceid, service, plot_volume_message, tags)).delay()
+    (is_3d_fits_file.s(content) | plot_volume.s() | upload.s(filebase + '.volume.png', graceid, service, plot_volume_message, tags)).delay()
 
 
 
 @app.task(queue='openmp')
 def bayestar(coinc_psd, graceid, service):
-    import os
-    from io import BytesIO
     from lalinference.io.events import ligolw
     from lalinference.io import fits
     from lalinference.bayestar.command import TemporaryDirectory
@@ -77,8 +107,8 @@ def bayestar(coinc_psd, graceid, service):
 
     # Parse event
     coinc, psd = coinc_psd
-    coinc = BytesIO(coinc)
-    psd = BytesIO(psd)
+    coinc = io.BytesIO(coinc)
+    psd = io.BytesIO(psd)
     event, = ligolw.open(coinc, psd_file=psd, coinc_def=None).values()
 
     # Run BAYESTAR
@@ -94,52 +124,85 @@ def bayestar(coinc_psd, graceid, service):
 @app.task
 def download(filename, graceid, service):
     """Download a file from GraceDB."""
-    from ligo.gracedb.rest import GraceDb
     return GraceDb(service).files(graceid, filename, raw=True).read()
 
 
 @app.task(ignore_result=True)
 def upload(filecontents, filename, graceid, service, message, tags):
     """Upload a file to GraceDB."""
-    from ligo.gracedb.rest import GraceDb
-    if filecontents is None:
-        return
-    log.info('uploading to gracedb: %s %s', graceid, filename)
     GraceDb(service).writeLog(graceid, message, filename, filecontents, tags)
+
+
+@app.task
+def fits_header(filename, filecontents):
+    """Plot a Mollweide projection of a sky map."""
+    with NamedTemporaryFile(content=filecontents) as fitsfile, \
+         astropy.io.fits.open(fitsfile.name) as hdus:
+        out = six.StringIO()
+        print('<!DOCTYPE html>', file=out)
+        print('<html lang="en">', file=out)
+        print('<head>', file=out)
+        print('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">', file=out)
+        print('<title>FITS headers for ', filename, '</title>', sep='', file=out)
+        print('</head>', file=out)
+        print('<body>', file=out)
+        print('<div class=container>', file=out)
+        print('<h1>FITS headers for ', filename, '</h1>', sep='', file=out)
+        print('<table class="table table-condensed table-striped">', file=out)
+        print('<thead>', file=out)
+        print('<tr>', file=out)
+        print('<th>Keyword</th>', file=out)
+        print('<th>Value</th>', file=out)
+        print('<th>Comment</th>', file=out)
+        print('</tr>', file=out)
+        print('</thead>', file=out)
+        print('<tbody>', file=out)
+        for ihdu, hdu in enumerate(hdus):
+            print('<tr class="info"><td colspan=3><strong>HDU #', ihdu, ' in ',
+                  filename, '</strong></td></tr>', sep='', file=out)
+            for card in hdu.header.cards:
+                print('<tr>', file=out)
+                print('<td style="font-family: monospace">', card.keyword, '</td>', sep='', file=out)
+                if card.keyword in ('COMMENT', 'HISTORY'):
+                    print('<td colspan=2>', card.value, '</td>', sep='', file=out)
+                else:
+                    print('<td style="font-family: monospace">', card.value, '</td>', sep='', file=out)
+                    print('<td>', card.comment, '</td>', sep='', file=out)
+                print('</tr>', file=out)
+        print('</tbody>', file=out)
+        print('</table>', file=out)
+        print('</div>', file=out)
+        print('</body>', file=out)
+        print('</html>', file=out)
+    return out.getvalue()
 
 
 @app.task
 def plot_allsky(filecontents):
     """Plot a Mollweide projection of a sky map."""
     from lalinference.scripts.bayestar_plot_allsky import main
-    with NamedTemporaryFile(mode='wb') as fitsfile, \
+    with NamedTemporaryFile(content=filecontents) as fitsfile, \
          NamedTemporaryFile(mode='rb', suffix='.png') as pngfile:
-        fitsfile.write(filecontents)
-        fitsfile.flush()
         main([fitsfile.name, '-o', pngfile.name, '--annotate',
               '--contour', '50', '90'])
         return pngfile.read()
 
 
-def is_3d_fits_file(f):
-    from astropy.io.fits import getval
+@app.task(raises=ValueError)
+def is_3d_fits_file(filecontents):
     try:
-        if getval(f, 'TUNIT4', 1) == 'Mpc-2':
-            return True
+        with NamedTemporaryFile(content=filecontents) as fitsfile:
+            if astropy.io.fits.getval(fitsfile.name, 'TUNIT4', 1) == 'Mpc-2':
+                return filecontents
     except (KeyError, IndexError):
-        pass
-    return False
+        raise ValueError('Not a 3D FITS file')
 
 
 @app.task(queue='openmp')
 def plot_volume(filecontents):
     """Plot a Mollweide projection of a sky map."""
     from lalinference.scripts.bayestar_plot_volume import main
-    with NamedTemporaryFile(mode='wb') as fitsfile, \
+    with NamedTemporaryFile(content=filecontents) as fitsfile, \
          NamedTemporaryFile(mode='rb', suffix='.png') as pngfile:
-        fitsfile.write(filecontents)
-        fitsfile.flush()
-        if not is_3d_fits_file(fitsfile.name):
-            return None
         main([fitsfile.name, '-o', pngfile.name, '--annotate'])
         return pngfile.read()
