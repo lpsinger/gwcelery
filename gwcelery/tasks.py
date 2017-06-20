b507e845a71f6b74dbb4f749526e549f04adf87c
Leo Singer <leo.singer@ligo.org>
2017-06-20 03:17:12 -0400

    Add BAYESTAR task

--- gwcelery/tasks.py
+++ gwcelery/tasks.py
@@ -1,20 +1,14 @@
 # Standard library imports
-import json
-import os
 from tempfile import NamedTemporaryFile
 
 # Third-party imports
-import astropy.io.fits
-from celery import Celery, chain, group
+from celery import Celery, group
 from celery.utils.log import get_task_logger
-from lalinference.scripts import bayestar_plot_allsky
-from lalinference.scripts import bayestar_plot_volume
-from ligo.gracedb.rest import GraceDb
 
 
 # Celery application object.
 # Use pickle serializer, because it supports byte values.
-app = Celery('tasks', backend='rpc://', broker='pyamqp://', config_source=dict(
+app = Celery('tasks', backend='redis://', broker='redis://', config_source=dict(
     accept_content=['json', 'pickle'],
     event_serializer='json',
     result_serializer='pickle',
@@ -28,6 +22,9 @@ log = get_task_logger(__name__)
 @app.task(ignore_result=True)
 def dispatch(payload):
     """Parse an LVAlert message and dispatch it to other tasks."""
+    import json
+    import os
+
     # Parse JSON payload
     alert = json.loads(payload)
 
@@ -50,7 +47,7 @@ def dispatch(payload):
         if fitsext:
             annotate_fits(versioned_filename, filebase, graceid, service, tags)
         elif filename == 'psd.xml.gz':
-            (bayestar.s(graceid, service) | upload('bayestar.fits.gz', graceid, service, 'sky localization complete', 'sky_loc')).delay()
+            (group(download.s('coinc.xml', graceid, service), download.s('psd.xml.gz', graceid, service)) | bayestar.s(graceid, service) | upload.s('bayestar.fits.gz', graceid, service, 'sky localization complete', 'sky_loc')).delay()
 
 
 def annotate_fits(versioned_filename, filebase, graceid, service, tags):
@@ -70,19 +67,41 @@ def annotate_fits(versioned_filename, filebase, graceid, service, tags):
 
 
 @app.task(queue='openmp')
-def bayestar(graceid, service):
-    pass
+def bayestar(coinc_psd, graceid, service):
+    import os
+    from io import BytesIO
+    from lalinference.io.events import ligolw
+    from lalinference.io import fits
+    from lalinference.bayestar.command import TemporaryDirectory
+    from lalinference.bayestar.sky_map import localize, rasterize
+
+    # Parse event
+    coinc, psd = coinc_psd
+    coinc = BytesIO(coinc)
+    psd = BytesIO(psd)
+    event, = ligolw.open(coinc, psd_file=psd, coinc_def=None).values()
+
+    # Run BAYESTAR
+    skymap = rasterize(localize(event))
+    skymap.meta['objid'] = str(graceid)
+
+    with TemporaryDirectory() as tmpdir:
+        fitspath = os.path.join(tmpdir, 'bayestar.fits.gz')
+        fits.write_sky_map(fitspath, skymap, nest=True)
+        return open(fitspath, 'rb').read()
 
 
 @app.task
 def download(filename, graceid, service):
     """Download a file from GraceDB."""
+    from ligo.gracedb.rest import GraceDb
     return GraceDb(service).files(graceid, filename, raw=True).read()
 
 
 @app.task(ignore_result=True)
 def upload(filecontents, filename, graceid, service, message, tags):
     """Upload a file to GraceDB."""
+    from ligo.gracedb.rest import GraceDb
     if filecontents is None:
         return
     log.info('uploading to gracedb: %s %s', graceid, filename)
@@ -92,19 +111,21 @@ def upload(filecontents, filename, graceid, service, message, tags):
 @app.task
 def plot_allsky(filecontents):
     """Plot a Mollweide projection of a sky map."""
+    from lalinference.scripts.bayestar_plot_allsky import main
     with NamedTemporaryFile(mode='wb') as fitsfile, \
          NamedTemporaryFile(mode='rb', suffix='.png') as pngfile:
         fitsfile.write(filecontents)
         fitsfile.flush()
-        bayestar_plot_allsky.main([
+        main([
             fitsfile.name, '-o', pngfile.name, '--annotate',
             '--contour', '50', '90'])
         return pngfile.read()
 
 
 def is_3d_fits_file(f):
+    from astropy.io.fits import getval
     try:
-        if astropy.io.fits.getval(f, 'TUNIT4', 1) == 'Mpc-2':
+        if getval(f, 'TUNIT4', 1) == 'Mpc-2':
             return True
     except (KeyError, IndexError):
         pass
@@ -114,12 +135,13 @@ def is_3d_fits_file(f):
 @app.task(queue='openmp')
 def plot_volume(filecontents):
     """Plot a Mollweide projection of a sky map."""
+    from lalinference.scripts import bayestar_plot_volume
     with NamedTemporaryFile(mode='wb') as fitsfile, \
          NamedTemporaryFile(mode='rb', suffix='.png') as pngfile:
         fitsfile.write(filecontents)
         fitsfile.flush()
         if not is_3d_fits_file(fitsfile.name):
             return None
-        bayestar_plot_volume.main([
+        lalinference.scripts.bayestar_plot_volume.main([
             fitsfile.name, '-o', pngfile.name, '--annotate'])
         return pngfile.read()
