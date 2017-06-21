b50d942d96c7d51ee52b7d7270200698bf9ac8d6
Leo Singer <leo.singer@ligo.org>
2017-06-20 21:34:16 -0400

    Add logging to BAYESTAR

--- gwcelery/tasks/bayestar.py
+++ gwcelery/tasks/bayestar.py
@@ -1,7 +1,10 @@
 import io
+import logging
 import os
 
 from ..celery import app
+
+log = logging.getLogger('BAYESTAR')
 
 
 @app.task(queue='openmp')
@@ -10,18 +13,33 @@ def bayestar(coinc_psd, graceid, service):
     from lalinference.io import fits
     from lalinference.bayestar.command import TemporaryDirectory
     from lalinference.bayestar.sky_map import localize, rasterize
+    from ligo.gracedb.logging import GraceDbLogHandler
+    from ligo.gracedb.rest import GraceDb
 
-    # Parse event
-    coinc, psd = coinc_psd
-    coinc = io.BytesIO(coinc)
-    psd = io.BytesIO(psd)
-    event, = ligolw.open(coinc, psd_file=psd, coinc_def=None).values()
+    handler = GraceDbLogHandler(GraceDb(service), graceid)
+    handler.setLevel(logging.INFO)
+    log.addHandler(handler)
 
-    # Run BAYESTAR
-    skymap = rasterize(localize(event))
-    skymap.meta['objid'] = str(graceid)
+    try:
+        # A little bit of Cylon humor
+        log.info('by your command...')
 
-    with TemporaryDirectory() as tmpdir:
-        fitspath = os.path.join(tmpdir, 'bayestar.fits.gz')
-        fits.write_sky_map(fitspath, skymap, nest=True)
-        return open(fitspath, 'rb').read()
+        # Parse event
+        coinc, psd = coinc_psd
+        coinc = io.BytesIO(coinc)
+        psd = io.BytesIO(psd)
+        event, = ligolw.open(coinc, psd_file=psd, coinc_def=None).values()
+
+        # Run BAYESTAR
+        log.info("starting sky localization")
+        skymap = rasterize(localize(event))
+        skymap.meta['objid'] = str(graceid)
+        log.info("sky localization complete")
+
+        with TemporaryDirectory() as tmpdir:
+            fitspath = os.path.join(tmpdir, 'bayestar.fits.gz')
+            fits.write_sky_map(fitspath, skymap, nest=True)
+            return open(fitspath, 'rb').read()
+    finally:
+        log.removeHandler(handler)
+        del handler
