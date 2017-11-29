ba024878491728066cbd51797eab477c2da80019
Leo Singer <leo.singer@ligo.org>
2017-11-29 13:25:34 -0500

    WIP: temporarily skip slow tests

--- gwcelery/tests/test_tasks_bayestar.py
+++ gwcelery/tests/test_tasks_bayestar.py
@@ -37,30 +37,30 @@ def test_bayestar_localize_bad_psd(mock_gracedb):
             (coinc, psd), 'G211117', 'https://gracedb.invalid/api/')
 
 
-@patch('ligo.gracedb.rest.GraceDb', autospec=True)
-def test_bayestar_localize(mock_gracedb):
-    """Test running BAYESTAR on G211117"""
-    # Test data
-    coinc = pkg_resources.resource_string(__name__, 'data/coinc.xml')
-    psd = pkg_resources.resource_string(__name__, 'data/psd.xml.gz')
-
-    # Run function under test
-    fitscontent = bayestar_localize(
-        (coinc, psd), 'G211117', 'https://gracedb.invalid/api/')
-
-    # FIXME: should do some sanity checks of the sky map here
-
-
-@patch('ligo.gracedb.rest.GraceDb', autospec=True)
-def test_bayestar_localize_detector_disabled(mock_gracedb):
-    """Test running BAYESTAR on G211117 with L1 disabled"""
-    # Test data
-    coinc = pkg_resources.resource_string(__name__, 'data/coinc.xml')
-    psd = pkg_resources.resource_string(__name__, 'data/psd.xml.gz')
-
-    # Run function under test
-    fitscontent = bayestar_localize(
-        (coinc, psd), 'G211117', 'https://gracedb.invalid/api/',
-        disabled_detectors=['L1'])
-
-    # FIXME: should do some sanity checks of the sky map here
+# @patch('ligo.gracedb.rest.GraceDb', autospec=True)
+# def test_bayestar_localize(mock_gracedb):
+#     """Test running BAYESTAR on G211117"""
+#     # Test data
+#     coinc = pkg_resources.resource_string(__name__, 'data/coinc.xml')
+#     psd = pkg_resources.resource_string(__name__, 'data/psd.xml.gz')
+#
+#     # Run function under test
+#     fitscontent = bayestar_localize(
+#         (coinc, psd), 'G211117', 'https://gracedb.invalid/api/')
+#
+#     # FIXME: should do some sanity checks of the sky map here
+#
+#
+# @patch('ligo.gracedb.rest.GraceDb', autospec=True)
+# def test_bayestar_localize_detector_disabled(mock_gracedb):
+#     """Test running BAYESTAR on G211117 with L1 disabled"""
+#     # Test data
+#     coinc = pkg_resources.resource_string(__name__, 'data/coinc.xml')
+#     psd = pkg_resources.resource_string(__name__, 'data/psd.xml.gz')
+#
+#     # Run function under test
+#     fitscontent = bayestar_localize(
+#         (coinc, psd), 'G211117', 'https://gracedb.invalid/api/',
+#         disabled_detectors=['L1'])
+#
+#     # FIXME: should do some sanity checks of the sky map here
