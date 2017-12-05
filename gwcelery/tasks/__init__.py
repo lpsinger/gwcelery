b633f9d62f97c0108472d0c0bb02934a358ee19d
Leo Singer <leo.singer@ligo.org>
2017-12-05 14:06:55 -0500

    Automatically import all task submodules
    All of the submodules containing GWCelery tasks must be imported
    so that they are discovered and registered on the application.
    
    This patch scans the directory and imports submodules automatically
    so that the imports do not have to be directly maintained.

--- gwcelery/tasks/__init__.py
+++ gwcelery/tasks/__init__.py
@@ -1,1 +1,12 @@
-from . import bayestar, dispatch, gracedb, lvalert, skymaps, voevent
+from __future__ import absolute_import
+import importlib
+import os
+import pkgutil
+
+# Import all submodules of this module
+modules = vars()
+for _, module, _ in pkgutil.iter_modules([os.path.dirname(__file__)]):
+    modules[module] = importlib.import_module('.' + module, __name__)
+
+# Clean up
+del importlib, module, modules, os, pkgutil
