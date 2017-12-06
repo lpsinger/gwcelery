aee6dd295799fddb41feb3cb2e5f2fd32beba66a
Leo Singer <leo.singer@ligo.org>
2017-12-05 20:02:56 -0500

    Fix htcondor submit file link

--- doc/conf.py
+++ doc/conf.py
@@ -110,7 +110,7 @@ html_theme_options = {'github_button': False, 'logo': 'logo.png'}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '../etc']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
--- doc/htcondor.md
+++ doc/htcondor.md
@@ -2,7 +2,7 @@
 
 The recommended way to start and stop GWCelery on the LIGO Data Grid cluster is
 using [HTCondor](https://research.cs.wisc.edu/htcondor/). See the example
-HTCondor submit file [`etc/gwcelery.sub`](etc/gwcelery.sub). This submit file
+HTCondor submit file [`gwcelery.sub`](_static/gwcelery.sub). This submit file
 will start up Redis, the three worker processes, and Flower. To start, go into
 the `etc/` directory in the source tree and run `condor_submit` as follows:
 
