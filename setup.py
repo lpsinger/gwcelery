689c371a4e4d2b501a0c66b28eaed0d484596900
Leo Singer <leo.singer@ligo.org>
2017-12-06 19:06:26 -0500

    Revert "Revert "Remove setuptools bootstrap script""
    This reverts commit c366a8bb54e463ec164814c0350205cf9b4fb780.
    
    ez_setup.py won't upgrade the old version of setuptools that we
    have anyway!

--- setup.py
+++ setup.py
@@ -1,14 +1,7 @@
-# Bootstrap setuptools installation.
-setup_requires = ['setuptools >= 30.3.0']
-try:
-    import pkg_resources
-    pkg_resources.require(setup_requires)
-except:
-    from ez_setup import use_setuptools
-    use_setuptools()
-
 import sys
 from setuptools import setup
+
+setup_requires = ['setuptools >= 30.3.0']
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
     setup_requires.append('pytest-runner')
 if {'build_sphinx'}.intersection(sys.argv):
