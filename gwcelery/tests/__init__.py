3f18d05124ed8a2dae86c2746d270282d072ecfa
Leo Singer <leo.singer@ligo.org>
2018-03-03 13:46:18 -0500

    Propagate exceptions in tests

--- gwcelery/tests/__init__.py
+++ gwcelery/tests/__init__.py
@@ -18,6 +18,7 @@ def celeryconf():
         gcn_bind_port=53410,
         gcn_remote_address='127.0.0.1',
         task_always_eager=True,
+        task_eager_propagates=True,
         lvalert_host='lvalert.invalid'
     )
     tmp = {key: app.conf[key] for key in new_conf.keys()}
