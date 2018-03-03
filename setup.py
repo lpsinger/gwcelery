9e183e53ff73d661b3b28b07491764fb4a87af23
Leo Singer <leo.singer@ligo.org>
2018-03-03 15:11:06 -0500

    Depend on correct version of celery_eternal for docs

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup_requires = ['setuptools >= 30.3.0']
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
     setup_requires.append('pytest-runner')
 if {'build_sphinx'}.intersection(sys.argv):
-    setup_requires.extend(['celery_eternal', 'recommonmark', 'sphinx'])
+    setup_requires.extend(['celery_eternal>=0.0.2', 'recommonmark', 'sphinx'])
 
 setup(setup_requires=setup_requires)
