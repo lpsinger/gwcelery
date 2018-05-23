5a79e5941b92a7ecb725f6def2f1828ed6858876
Leo Singer <leo.singer@ligo.org>
2018-05-23 12:30:33 -0400

    Set up fake GraceDb client for all tests

--- gwcelery/tests/conftest.py
+++ gwcelery/tests/conftest.py
@@ -1,3 +1,6 @@
+import unittest
+
+from ligo.gracedb import rest
 import pytest
 from pytest_socket import disable_socket
 
@@ -14,12 +17,21 @@ def celeryconf():
         gcn_remote_address='127.0.0.1',
         task_always_eager=True,
         task_eager_propagates=True,
-        lvalert_host='lvalert.invalid'
+        lvalert_host='lvalert.invalid',
+        gracedb_host='gracedb.invalid'
     )
     tmp = {key: app.conf[key] for key in new_conf.keys()}
     app.conf.update(new_conf)
     yield
     app.conf.update(tmp)
+
+
+@pytest.fixture(autouse=True)
+def fake_gracedb_client(monkeypatch):
+    mock_client = unittest.mock.create_autospec(rest.GraceDb)
+    mock_client.service_url = 'https://gracedb.invalid/api/'
+    monkeypatch.setattr('gwcelery.tasks.gracedb.client', mock_client)
+    yield
 
 
 def pytest_runtest_setup():
