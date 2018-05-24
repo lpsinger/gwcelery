67e5b75f5f37bab755487f60b56c1cf0f746e22c
Min-A Cho <min-a.cho@ligo.org>
2018-05-23 20:38:35 -0700

    passing the global gracedb client to the compose function; this requires version 0.0.7 of ligo-followup-advocate

--- gwcelery/tasks/circulars.py
+++ gwcelery/tasks/circulars.py
@@ -2,9 +2,10 @@
 import ligo.followup_advocate
 
 from ..celery import app
+from . import gracedb
 
 
 @app.task(shared=False)
 def create_circular(graceid):
     """Create and return circular txt."""
-    return ligo.followup_advocate.compose(graceid)
+    return ligo.followup_advocate.compose(graceid, client=gracedb.client)
