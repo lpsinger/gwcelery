7f6b57ae60f798588b7fd6a4d0f1b50e410abf38
Leo Singer <leo.singer@ligo.org>
2018-05-31 22:10:27 -0400

    Complete unit tests for !58
    - The mock GraceDb object was overspecified: some methods and
      return values were mocked up even though they were not ussed.
    - There is now a unit test that exercises the branch for replacing
      an existing event, bringing us to 100% test coverage on the new
      `gwcelery.tasks.external_triggers` module.

--- gwcelery/tests/test_tasks_external_triggers.py
+++ gwcelery/tests/test_tasks_external_triggers.py
@@ -7,12 +7,19 @@ from pkg_resources import resource_string
 from ..tasks.gcn.external_triggers import handle_exttrig
 
 
-@patch('gwcelery.tasks.gracedb.create_event', return_value='T0446')
-@patch('gwcelery.tasks.gracedb.upload')
-def test_handle_exttrig(mock_upload, mock_create_event):
+@patch('gwcelery.tasks.gracedb.create_event')
+def test_handle_exttrig_create_event(mock_create_event):
     text = resource_string(__name__, 'data/fermi_grb_gcn.xml')
     handle_exttrig(payload=text)
     mock_create_event.assert_called_once_with(filecontents=text,
                                               search='GRB',
                                               pipeline='Fermi',
                                               group='Test')
+
+
+@patch('gwcelery.tasks.gracedb.get_events', return_value=[{'graceid': 'E1'}])
+@patch('gwcelery.tasks.gracedb.replace_event')
+def test_handle_exttrig_replace_event(mock_replace_event, mock_get_events):
+    text = resource_string(__name__, 'data/fermi_grb_gcn.xml')
+    handle_exttrig(payload=text)
+    mock_replace_event.assert_called_once_with('E1', text)
