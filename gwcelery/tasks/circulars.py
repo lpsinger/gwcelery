5932d49f713949a7179e4bb5263039dcd147defc
Min-A Cho <min-a.cho@ligo.org>
2018-07-12 06:57:32 -0700

    retry building and uploading circulars in case of gracedb timeout errors

--- gwcelery/tasks/circulars.py
+++ gwcelery/tasks/circulars.py
@@ -1,11 +1,10 @@
 """Generate and upload automated circulars."""
 import ligo.followup_advocate
 
-from ..celery import app
 from . import gracedb
 
 
-@app.task(shared=False)
+@gracedb.task(shared=False)
 def create_circular(graceid):
     """Create and return circular txt."""
     return ligo.followup_advocate.compose(graceid, client=gracedb.client)
