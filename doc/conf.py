f7c656a8797ce12a4642b9211782e3eb948fc3ee
Leo Singer <leo.singer@ligo.org>
2018-07-27 10:47:51 -0400

    Restore broken task autodocs

--- doc/conf.py
+++ doc/conf.py
@@ -122,7 +122,7 @@ for mod in ['astropy',
         importlib.import_module(mod)
     except ImportError:
         autodoc_mock_imports.append(mod)
-autodoc_default_flags = ['members', 'show-inheritance']
+autodoc_default_flags = ['members', 'undoc-members', 'show-inheritance']
 autosummary_generate = True
 
 
