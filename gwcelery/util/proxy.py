465b425935840dcbf30dab729982f21648929db7
Leo Singer <leo.singer@ligo.org>
2018-08-01 09:12:03 -0400

    Exclude sphinx patch from coverage

--- gwcelery/util/proxy.py
+++ gwcelery/util/proxy.py
@@ -11,7 +11,7 @@ __all__ = ('PromiseProxy',)
 prog = os.path.basename(sys.argv[0])
 if prog != 'sphinx-build' and 'build_sphinx' not in sys.argv:
     from celery.local import PromiseProxy
-else:
+else:  # pragma: no cover
     class PromiseProxy:
 
         def __init__(self, *args, **kwargs):
