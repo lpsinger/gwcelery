9e9f10282b7e215c986a18daecfc7bada4cad3e1
Leo Singer <leo.singer@ligo.org>
2018-08-01 09:36:28 -0400

    Add unit tests for gwcelery condor resubmit

--- gwcelery/tests/test_condor.py
+++ gwcelery/tests/test_condor.py
@@ -51,3 +51,45 @@ def test_condor_submit_already_running(mock_execvp, mock_check_output):
     mock_check_output.assert_called_once_with(
         ('condor_q', '-xml') + condor.CONSTRAINTS)
     mock_execvp.assert_not_called()
+
+
+class MockMonotonic:
+    """Mock :meth:`time.monotonic` to speed up the apparent passage of time."""
+
+    def __init__(self):
+        self._t = 0.0
+
+    def __call__(self):
+        self._t += 1.0
+        return self._t
+
+
+@mock.patch('time.sleep')
+@mock.patch('time.monotonic', new_callable=MockMonotonic)
+@mock.patch('subprocess.check_output',
+            return_value=b'<classads><c></c></classads>')
+@mock.patch('subprocess.check_call')
+def test_condor_resubmit_fail(mock_check_call, _, __, ___):
+    """Test that ``gwcelery condor resubmit`` fails if we are unable to
+    ``condor_rm`` the jobs."""
+    try:
+        app.start(['gwcelery', 'condor', 'resubmit'])
+    except SystemExit as e:
+        assert e.code == 1
+    mock_check_call.assert_called_with(('condor_rm',) + condor.CONSTRAINTS)
+
+
+@mock.patch('subprocess.check_output',
+            return_value=b'<classads></classads>')
+@mock.patch('subprocess.check_call')
+@mock.patch('os.execvp', side_effect=SystemExit(0))
+def test_condor_resubmit_succeeds(mock_execvp, mock_check_call, _):
+    """Test that ``gwcelery condor resubmit`` fails if we are unable to
+    ``condor_rm`` the jobs."""
+    try:
+        app.start(['gwcelery', 'condor', 'resubmit'])
+    except SystemExit as e:
+        assert e.code == 0
+    mock_check_call.assert_not_called()
+    mock_execvp.assert_called_once_with(
+        'condor_submit', ('condor_submit', condor.SUBMIT_FILE))
