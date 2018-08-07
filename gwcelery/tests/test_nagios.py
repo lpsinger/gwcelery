b2a7caec5bd34e287088fc7066501ca047b2a7d4
Leo Singer <leo.singer@ligo.org>
2018-08-07 00:14:42 -0400

    Add unit tests for Nagios

---
+++ gwcelery/tests/test_nagios.py
@@ -0,0 +1,53 @@
+import subprocess
+
+import pytest
+
+from .. import app
+from .. import nagios
+
+try:
+    subprocess.check_call(['which', 'redis-server'])
+except subprocess.CalledProcessError:
+    HAS_REDIS = False
+else:
+    HAS_REDIS = True
+
+
+@pytest.fixture
+def unix_domain_socket(monkeypatch, socket_enabled, tmpdir):
+    filename = str(tmpdir / 'redis.sock')
+    monkeypatch.setenv('CELERY_BROKER_URL', 'redis+socket://' + filename)
+    yield filename
+
+
+@pytest.fixture
+def redis(unix_domain_socket):
+    with subprocess.Popen(['redis-server', '--port', '0',
+                           '--unixsocket', unix_domain_socket,
+                           '--unixsocketperm', '700']) as p:
+        yield p
+        p.terminate()
+
+
+# Note: the names of these unit tests must be very short because there is a
+# very strict limit on the length of a Unix domain socket path.
+# See https://unix.stackexchange.com/questions/367008
+def test0(capsys, unix_domain_socket):
+    """Test that we generate the correct message when the broker is not
+    running."""
+    with pytest.raises(SystemExit) as excinfo:
+        app.start(['gwcelery', 'nagios'])
+    assert excinfo.value.code == nagios.NagiosPluginStatus.CRITICAL
+    out, err = capsys.readouterr()
+    assert out.startswith('CRITICAL: No connection to broker')
+
+
+@pytest.mark.skipif(not HAS_REDIS, reason='redis-server is not installed')
+def test1(capsys, redis):
+    """Test that we generate the correct message when the broker is running but
+    none of the workers are."""
+    with pytest.raises(SystemExit) as excinfo:
+        app.start(['gwcelery', 'nagios'])
+    assert excinfo.value.code == nagios.NagiosPluginStatus.CRITICAL
+    out, err = capsys.readouterr()
+    assert out.startswith('CRITICAL: Not all expected queues are active')
