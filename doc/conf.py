04456799395b2b2c493728bd7ff751387c1aaa7b
Leo Singer <leo.singer@ligo.org>
2018-08-15 13:57:52 -0400

    Reorder imports

--- doc/conf.py
+++ doc/conf.py
@@ -17,8 +17,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
 import importlib
+import os
 from subprocess import check_output
 import sys
 
