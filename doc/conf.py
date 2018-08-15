e256b1d03f53a6e6114652bfec0d9a708ee1160e
Leo Singer <leo.singer@ligo.org>
2018-08-15 14:56:59 -0400

    Revert "Add proper gitlab links"
    This reverts commit dc4d285833bd0fde9822925ca405baa439bfced5.

--- doc/conf.py
+++ doc/conf.py
@@ -149,14 +149,6 @@ html_theme_options = {
     'logo_name': True
 }
 
-# Integrate GitLab readthedocs links
-html_context = {
-    'display_gitlab': True,
-    'gitlab_host': 'git.ligo.org',
-    'gitlab_user': 'emfollow',
-    'gitlab_repo': 'gwcelery'
-}
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
