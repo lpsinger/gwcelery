1d79e1c330e66392767d330975d1fea7cf71da8d
Leo Singer <leo.singer@ligo.org>
2018-08-15 13:23:37 -0400

    Move requirements to requirements.txt files
    This will make it easier to build on readthedocs.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+import pkg_resources
 import sys
 from setuptools import setup
 
+
+def get_requirements(filename):
+    with open(filename, 'r') as f:
+        return [str(r) for r in pkg_resources.parse_requirements(f)]
+
+
 setup_requires = ['setuptools >= 30.3.0']
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
-    setup_requires.append('pytest-runner')
+    setup_requires.extend(get_requirements('test-requirements.txt'))
 if {'build_sphinx'}.intersection(sys.argv):
-    setup_requires.extend(['celery>=4.2.0', 'celery_eternal>=0.0.2',
-                           'recommonmark', 'sphinx'])
+    setup_requires.extend(get_requirements('docs-requirements.txt'))
 
-setup(setup_requires=setup_requires)
+setup(install_requires=get_requirements('requirements.txt'),
+      setup_requires=setup_requires)
