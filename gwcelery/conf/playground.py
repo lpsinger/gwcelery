62255fda428022f6aadc33837f0913a4b90c200a
Leo Singer <leo.singer@ligo.org>
2018-11-26 12:12:51 -0500

    Fix Sphinx syntax errors

--- gwcelery/conf/playground.py
+++ gwcelery/conf/playground.py
@@ -12,10 +12,10 @@ frame_types = {'H1': 'H1_O2_llhoft',
                'L1': 'L1_O2_llhoft',
                'V1': 'V1_O2_llhoft'}
 """Types of frames used in Parameter Estimation with LALInference (see
-:module:`gwcelery.tasks.lalinference`)"""
+:mod:`gwcelery.tasks.lalinference`)"""
 
 channel_names = {'H1': 'H1:GDS-CALIB_STRAIN_O2Replay',
                  'L1': 'L1:GDS-CALIB_STRAIN_O2Replay',
                  'V1': 'V1:Hrec_hoft_16384Hz_O2Replay'}
 """Names of h(t) channels used in Parameter Estimation with LALInference (see
-:module:`gwcelery.tasks.lalinference`)"""
+:mod:`gwcelery.tasks.lalinference`)"""
