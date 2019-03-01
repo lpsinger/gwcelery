f2b37958bbc5c87701fddbe05ee3b15820f82fc9
Leo Singer <leo.singer@ligo.org>
2019-03-01 13:55:58 -0500

    Silence ligo-proxy-init

--- renew-cert.sh
+++ renew-cert.sh
@@ -6,7 +6,7 @@ X509_USER_KEY="$HOME/.globus/userkey.pem"
 KERBEROS_KEYTAB="${HOME}/.globus/krb5.keytab"
 KERBEROS_PRINCIPAL="$(klist -k "${KERBEROS_KEYTAB}" | tail -n 1 | sed 's/.*\s//')"
 kinit "${KERBEROS_PRINCIPAL}" -k -t "${KERBEROS_KEYTAB}"
-ligo-proxy-init -k
+ligo-proxy-init -k > /dev/null
 GRID_PROXY_PATH="$(grid-proxy-info -path)"
 cp "${GRID_PROXY_PATH}" "${X509_USER_CERT}"
 cp "${GRID_PROXY_PATH}" "${X509_USER_KEY}"
