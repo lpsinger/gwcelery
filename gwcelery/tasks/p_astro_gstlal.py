dae6a72b24c106c06729cabd68b46d26f1450ed7
Shasvath Jagat Kapadia <shasvath.kapadia@ldas-pcdev4.ligo.caltech.edu>
2019-03-28 13:12:45 -0700

    p_astro_gstlal.py indent fixes

--- gwcelery/tasks/p_astro_gstlal.py
+++ gwcelery/tasks/p_astro_gstlal.py
@@ -79,18 +79,19 @@ def noilwdchar(xmldoc):
     for table in xmldoc.getElementsByTagName(ligolw.Table.tagName):
     # first strip table names from column names that shouldn't
     # have them
-    if table.Name in lsctables.TableByName:
-        validcolumns = lsctables.TableByName[table.Name].validcolumns
-        stripped_column_to_valid_column = \
-            dict((ligolw_table.Column.ColumnName(name), name) 
-                for name in validcolumns)
-        for column in table.getElementsByTagName(ligolw.Column.tagName):
-            if column.getAttribute("Name") not in validcolumns:
-                before = column.getAttribute("Name")
-                column.setAttribute("Name", 
-                    stripped_column_to_valid_column[column.Name])
-                idattrs = tuple(table.columnnames[i] for i, coltype in 
-                    enumerate(table.columntypes) if coltype == u"ilwd:char")
+        if table.Name in lsctables.TableByName:
+            validcolumns = \
+                lsctables.TableByName[table.Name].validcolumns
+            stripped_column_to_valid_column = \
+                dict((ligolw_table.Column.ColumnName(name), name)
+                    for name in validcolumns)
+            for column in table.getElementsByTagName(ligolw.Column.tagName):
+                if column.getAttribute("Name") not in validcolumns:
+                    before = column.getAttribute("Name")
+                    column.setAttribute("Name",
+                        stripped_column_to_valid_column[column.Name])
+                    idattrs = tuple(table.columnnames[i] for i, coltype in
+                        enumerate(table.columntypes) if coltype == u"ilwd:char")
                 if not idattrs:
                     continue
                 for row in table:
