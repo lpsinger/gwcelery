3cfec6b6fb4452d3bacca4022d184901285a6b59
Leo Singer <leo.singer@ligo.org>
2019-04-18 11:24:31 -0400

    Revert "Revert "Make gwdatafind calls in detchar.create_cache more robust""
    This reverts commit 4a35884674a5ea8abd8ba3a9d085566b4f54b486.

--- gwcelery/conf/production.py
+++ gwcelery/conf/production.py
@@ -25,13 +25,17 @@ low_latency_frame_types = {'H1': 'H1_llhoft',
                            'L1': 'L1_llhoft',
                            'V1': 'V1_llhoft'}
 """Types of frames used in Parameter Estimation with LALInference (see
-:mod:`gwcelery.tasks.lalinference`)"""
+:mod:`gwcelery.tasks.lalinference`) and in cache creation for detchar
+checks (see :mod:`gwcelery.tasks.detchar`).
+"""
 
 high_latency_frame_types = {'H1': 'H1_HOFT_C00',
                             'L1': 'L1_HOFT_C00',
                             'V1': 'V1Online'}
-"""Types of nonllhoft-frames used in Parameter Estimation with LALInference
-(see :mod:`gwcelery.tasks.lalinference`)"""
+"""Types of high latency frames used in Parameter Estimation with LALInference
+(see :mod:`gwcelery.tasks.lalinference`) and in cache creation for detchar
+checks (see :mod:`gwcelery.tasks.detchar`).
+"""
 
 strain_channel_names = {'H1': 'H1:GDS-CALIB_STRAIN',
                         'L1': 'L1:GDS-CALIB_STRAIN',
