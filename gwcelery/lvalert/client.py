5320d522688e685139130831465d55dfa5d11031
Leo Singer <leo.singer@ligo.org>
2019-05-28 21:31:07 -0400

    Add log message on resubscribing to LVAlert nodes

--- gwcelery/lvalert/client.py
+++ gwcelery/lvalert/client.py
@@ -1,4 +1,7 @@
+from celery.utils.log import get_logger
 import sleek_lvalert
+
+log = get_logger(__name__)
 
 
 class LVAlertClient(sleek_lvalert.LVAlertClient):
@@ -9,5 +12,6 @@ class LVAlertClient(sleek_lvalert.LVAlertClient):
         self.add_event_handler('session_start', self._resubscribe)
 
     def _resubscribe(self, event):
+        log.info('Resubscribing to PubSub nodes')
         current_subscriptions = set(self.get_subscriptions())
         self.subscribe(*(self._needed_subscriptions - current_subscriptions))
