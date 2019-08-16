e1d80512f83fd646e4c357808ff248fff3e1ba0b
Leo Singer <leo.singer@ligo.org>
2019-08-16 15:12:23 -0400

    Revert "Retryable tasks for raven.py"
    This reverts commit 286fd87e65fab418b30bb77d9e8a3d18f0a34bf4.

--- gwcelery/tasks/raven.py
+++ gwcelery/tasks/raven.py
@@ -9,8 +9,7 @@ from . import gracedb
 from . import ligo_fermi_skymaps
 
 
-@app.task(shared=False)
-def calculate_coincidence_far(superevent_id, exttrig_id, preferred_id, group):
+def calculate_coincidence_far(gracedb_id, group):
     """Compute temporal coincidence FAR for external trigger and superevent
     coincidence by calling ligo.raven.search.calc_signif_gracedb.
 
@@ -23,17 +22,11 @@ def calculate_coincidence_far(superevent_id, exttrig_id, preferred_id, group):
         gracedb_id superevent
     """
     try:
-        preferred_skymap = ligo_fermi_skymaps.get_preferred_skymap(
-                               preferred_id)
+        preferred_skymap = ligo_fermi_skymaps.get_preferred_skymap(gracedb_id)
     except ValueError:
         preferred_skymap = None
-    try:
-        ext_skymap = gracedb.download('glg_healpix_all_bn_v00.fit',
-                                      exttrig_id)
-    except HTTPError as e:
-        if e.status == 404:
-            ext_skymap = None
 
+    em_events = gracedb.get_superevent(gracedb_id)['em_events']
     tl_cbc, th_cbc = app.conf['raven_coincidence_windows']['GRB_CBC']
     tl_burst, th_burst = app.conf['raven_coincidence_windows']['GRB_Burst']
 
@@ -43,27 +36,34 @@ def calculate_coincidence_far(superevent_id, exttrig_id, preferred_id, group):
         tl, th = tl_burst, th_burst
 
     canvas = chain()
-    canvas |= gracedb.get_search.si(exttrig_id)
-    if ext_skymap and preferred_skymap:
-        canvas |= calc_signif.s(superevent_id, exttrig_id, tl, th,
-                                incl_sky=True,
-                                se_fitsfile=preferred_skymap)
-    else:
-        canvas |= calc_signif.s(superevent_id, exttrig_id, tl, th,
-                                incl_sky=False)
+    for exttrig_id in em_events:
+        search = gracedb.get_event(exttrig_id)['search']
+        try:
+            ext_skymap = gracedb.download('glg_healpix_all_bn_v00.fit',
+                                          exttrig_id)
+        except HTTPError:
+            ext_skymap = None
+        if ext_skymap and preferred_skymap:
+            canvas |= (
+                calc_signif.si(gracedb_id, exttrig_id, tl, th,
+                               search,
+                               incl_sky=True,
+                               se_fitsfile=preferred_skymap))
+        else:
+            canvas |= (
+                calc_signif.si(gracedb_id, exttrig_id, tl, th, search,
+                               incl_sky=False))
     return canvas
 
 
 @app.task(shared=False)
-def calc_signif(search, se_id, exttrig_id, tl, th, incl_sky=False,
-                se_fitsfile=None):
+def calc_signif(se_id, exttrig_id, tl, th, search, incl_sky, se_fitsfile=None):
     """Calculate FAR of GRB exttrig-GW coincidence"""
     return ligo.raven.search.calc_signif_gracedb(
         se_id, exttrig_id, tl, th, grb_search=search, se_fitsfile=se_fitsfile,
         incl_sky=incl_sky, gracedb=gracedb.client)
 
 
-@app.task(shared=False)
 def coincidence_search(gracedb_id, alert_object, group=None, pipelines=[]):
     """Perform ligo-raven search for coincidences.
     The ligo.raven.search.search method applies EM_COINC label on its own.
@@ -98,11 +98,23 @@ def coincidence_search(gracedb_id, alert_object, group=None, pipelines=[]):
         raise ValueError('Invalid RAVEN search request for {0}'.format(
             gracedb_id))
 
-    (
-        search.si(gracedb_id, alert_object, tl, th, group, pipelines)
-        |
-        raven_pipeline.s(gracedb_id, alert_object, group)
-    ).delay()
+    raven_search_results = search(gracedb_id, alert_object,
+                                  tl, th, group, pipelines)
+    add_exttrig_to_superevent(raven_search_results, gracedb_id)
+
+    if gracedb_id.startswith('E') and raven_search_results:
+        for search_result in raven_search_results:
+            calculate_coincidence_far(
+                search_result['superevent_id'], group).delay()
+            gracedb.create_label.delay('EM_COINC',
+                                       search_result['superevent_id'])
+        gracedb.create_label.delay('EM_COINC', gracedb_id)
+
+    elif raven_search_results:
+        calculate_coincidence_far(gracedb_id, group).delay()
+        for search_result in raven_search_results:
+            gracedb.create_label.delay('EM_COINC', search_result['graceid'])
+        gracedb.create_label.delay('EM_COINC', gracedb_id)
 
 
 @app.task(shared=False)
@@ -141,11 +153,9 @@ def search(gracedb_id, alert_object, tl=-5, th=5, group=None,
 
 
 @app.task(shared=False)
-def raven_pipeline(raven_search_results, gracedb_id, alert_object, group):
-    """Executes much of the full raven pipeline, including adding
-    the external trigger to the superevent, calculating the
-    coincidence false alarm rate, and applys 'EM_COINC' to the
-    appropriate events.
+def add_exttrig_to_superevent(raven_search_results, gracedb_id):
+    """Add external trigger to the list of em_events after
+    ligo.raven.search.search finds a coincidence
 
     Parameters
     ----------
@@ -153,46 +163,13 @@ def raven_pipeline(raven_search_results, gracedb_id, alert_object, group):
         list of dictionaries of each related gracedb trigger
     gracedb_id: str
         ID of either a superevent or external trigger
-    group: str
-        Burst or CBC
     """
-
+    # First determine whether the gracedb_id is for a superevent or exttrig
     if gracedb_id.startswith('E'):
-        raven_search_results = preferred_superevent(raven_search_results)
-    for result in raven_search_results:
-        if gracedb_id.startswith('E'):
-            superevent_id = result['superevent_id']
-            exttrig_id = gracedb_id
-            preferred_gwevent_id = result['preferred_event']
-        else:
-            superevent_id = gracedb_id
-            exttrig_id = result['graceid']
-            preferred_gwevent_id = alert_object['preferred_event']
-
-        (
-            gracedb.add_event_to_superevent.si(superevent_id, exttrig_id)
-            |
-            calculate_coincidence_far.si(superevent_id, exttrig_id,
-                                         preferred_gwevent_id, group)
-            |
-            gracedb.create_label.si('EM_COINC', superevent_id)
-            |
-            gracedb.create_label.si('EM_COINC', exttrig_id)
-        ).delay()
-
-
-@app.task(shared=False)
-def preferred_superevent(raven_search_results):
-    """Chooses the superevent with the lowest far for an external
-    event to be added to. This is to prevent errors from trying to
-    add one external event to multiple superevents.
-
-    Parameters
-    ----------
-    raven_search_results: list
-        list of dictionaries of each related gracedb trigger
-    """
-
-    minfar, idx = min((result['far'], idx) for (idx, result) in
-                      enumerate(raven_search_results))
-    return [raven_search_results[idx]]
+        for superevent in raven_search_results:
+            superevent_id = superevent['superevent_id']
+            gracedb.add_event_to_superevent(superevent_id, gracedb_id)
+    else:
+        for exttrig in raven_search_results:
+            exttrig_id = exttrig['graceid']
+            gracedb.add_event_to_superevent(gracedb_id, exttrig_id)
