7cc79319e6a35536b78afebc80ac81171f0d0d07
Soichiro Morisaki <soichiro.morisaki@ligo.org>
2019-10-27 17:24:01 +0900

    tasks/inference.py: clean up inference.py
    - remove unnecessary blanck lines
    - separate codes to prepare lalinference DAG and bilby DAG
    - do not source conda environment for bilby since it is now base environment of
      gwcelery

--- gwcelery/tasks/inference.py
+++ gwcelery/tasks/inference.py
@@ -198,16 +198,140 @@ def pre_pe_tasks(event, superevent_id):
 
 
 @app.task(shared=False)
-def dag_prepare(coinc_psd, rundir, superevent_id, preferred_event_id,
-                pe_pipeline, ini_contents=None):
-    """Create a Condor DAG to run Parameter Estimation on a given event.
+def _setup_dag_for_lalinference(coinc_psd, ini_contents, rundir, superevent_id):
+    """Create DAG for a lalinference run and return the path to DAG.
 
     Parameters
     ----------
-    coinc_psd : tuple or json contents
-        For lalinference, tuple of the byte contents of
-        ``coinc.xml`` and ``psd.xml.gz``
-        For bilby, json contents retrieved from gracedb.get_event()
+    coinc_psd : tuple of byte contents
+        Tuple of the byte contents of ``coinc.xml`` and ``psd.xml.gz``
+    ini_contents : str
+        The content of online_lalinference_pe.ini
+    rundir : str
+        The path to a run directory where the DAG file exits
+    superevent_id : str
+        The GraceDB ID of a target superevent
+
+    Returns
+    -------
+    path_to_dag : str
+        The path to the .dag file
+    """
+    coinc_contents, psd_contents = coinc_psd
+
+    # write down coinc.xml in the run directory
+    path_to_coinc = os.path.join(rundir, 'coinc.xml')
+    with open(path_to_coinc, 'wb') as f:
+        f.write(coinc_contents)
+
+    # write down psd.xml.gz
+    if psd_contents is not None:
+        path_to_psd = os.path.join(rundir, 'psd.xml.gz')
+        with open(path_to_psd, 'wb') as f:
+            f.write(psd_contents)
+        psd_arg = ['--psd', path_to_psd]
+    else:
+        psd_arg = []
+
+    # write down .ini file in the run directory.
+    path_to_ini = os.path.join(rundir, ini_name)
+    with open(path_to_ini, 'w') as f:
+        f.write(ini_contents)
+
+    try:
+        subprocess.run(
+            ['lalinference_pipe', '--run-path', rundir,
+             '--coinc', path_to_coinc, path_to_ini] + psd_arg,
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError as e:
+        contents = b'args:\n' + json.dumps(e.args[1]).encode('utf-8') + \
+                   b'\n\nstdout:\n' + e.stdout + b'\n\nstderr:\n' + e.stderr
+        gracedb.upload.delay(
+            filecontents=contents, filename='pe_dag.log',
+            graceid=superevent_id,
+            message='Failed to prepare DAG for lalinference', tags='pe'
+        )
+        shutil.rmtree(rundir)
+        raise
+    else:
+        # Remove the ini file so that people do not accidentally use this ini
+        # file and hence online-PE-only nodes.
+        os.remove(path_to_ini)
+
+    return os.path.join(rundir, 'multidag.dag')
+
+
+@app.task(shared=False)
+def _setup_dag_for_bilby(event, rundir, preferred_event_id, superevent_id):
+    """Create DAG for a bilby run and return the path to DAG.
+
+    Parameters
+    ----------
+    event : json contents
+        The json contents retrieved from gracedb.get_event()
+    rundir : str
+        The path to a run directory where the DAG file exits
+    preferred_event_id : str
+        The GraceDB ID of a target preferred event
+    superevent_id : str
+        The GraceDB ID of a target superevent
+
+    Returns
+    -------
+    path_to_dag : str
+        The path to the .dag file
+    """
+    path_to_json = os.path.join(rundir, 'event.json')
+    with open(path_to_json, 'w') as f:
+        json.dump(event, f, indent=2)
+
+    path_to_webdir = os.path.join(
+        app.conf['pe_results_path'], preferred_event_id, 'bilby'
+    )
+
+    setup_arg = \
+        'bilby_pipe_gracedb --webdir {} --outdir {} --json {}'.format(
+            path_to_webdir, rundir, path_to_json)
+    if not app.conf['sentry_environment'] == 'production':
+        setup_arg += ' --channel-dict o2replay --sampler-kwargs FastTest'
+    try:
+        subprocess.run(
+            setup_arg, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError as e:
+        contents = b'args:\n' + json.dumps(e.args[1]).encode('utf-8') + \
+                   b'\n\nstdout:\n' + e.stdout + b'\n\nstderr:\n' + e.stderr
+        gracedb.upload.delay(
+            filecontents=contents, filename='pe_dag.log',
+            graceid=superevent_id,
+            message='Failed to prepare DAG for bilby', tags='pe'
+        )
+        shutil.rmtree(rundir)
+        raise
+    else:
+        # Uploads bilby ini file to GraceDB
+        _upload_result(rundir, 'bilby_config.ini', superevent_id,
+                       'Automatically generated Bilby configuration file',
+                       'pe', 'online_bilby_pe.ini').delay()
+
+    path_to_dag, = glob.glob(os.path.joins(rundir, 'submit/dag*.submit'))
+    return path_to_dag
+
+
+@app.task(shared=False)
+def _condor_no_submit(path_to_dag):
+    """Run 'condor_submit_dag -no_submit' and return the path to .sub file."""
+    subprocess.run(['condor_submit_dag', '-no_submit', path_to_dag])
+    return '{}.condor.sub'.format(path_to_dag)
+
+
+@app.task(shared=False)
+def dag_prepare_task(rundir, superevent_id, preferred_event_id, pe_pipeline,
+                     ini_contents=None):
+    """Return a canvas of tasks to prepare DAG.
+
+    Parameters
+    ----------
     rundir : str
         The path to a run directory where the DAG file exits
     superevent_id : str
@@ -216,112 +340,29 @@ def dag_prepare(coinc_psd, rundir, superevent_id, preferred_event_id,
         The GraceDB ID of a target preferred event
     pe_pipeline : str
         The parameter estimation pipeline used
-        Either lalinference OR bilby
+        Either 'lalinference' OR 'bilby'
     ini_contents : str
         The content of online_lalinference_pe.ini
+        Required if pe_pipeline == 'lalinference'
 
     Returns
     -------
-    submit_file : str
-        The path to the .sub file
+    canvas : canvas of tasks
+        The canvas of tasks to prepare DAG
     """
-
     if pe_pipeline == 'lalinference':
-
-        coinc_contents, psd_contents = coinc_psd
-
-        # write down coinc.xml in the run directory
-        path_to_coinc = os.path.join(rundir, 'coinc.xml')
-        with open(path_to_coinc, 'wb') as f:
-            f.write(coinc_contents)
-
-        # write down psd.xml.gz
-        if psd_contents is not None:
-            path_to_psd = os.path.join(rundir, 'psd.xml.gz')
-            with open(path_to_psd, 'wb') as f:
-                f.write(psd_contents)
-            psd_arg = '--psd {}'.format(path_to_psd)
-        else:
-            psd_arg = ''
-
-        # write down .ini file in the run directory.
-        path_to_ini = rundir + '/' + ini_name
-        with open(path_to_ini, 'w') as f:
-            f.write(ini_contents)
-
-        setup_arg = 'lalinference_pipe --run-path {} '.format(rundir) +\
-                    '--coinc {} '.format(path_to_coinc) +\
-                    '{} {}'.format(path_to_ini, psd_arg)
-
-        dag_arg = 'condor_submit_dag -no_submit {}/multidag.dag'.format(rundir)
-
-    if pe_pipeline == 'bilby':
-
-        path_to_json = os.path.join(rundir, 'event.json')
-        with open(path_to_json, 'w') as f:
-            json.dump(coinc_psd, f, indent=2)
-
-        path_to_webdir = os.path.join(
-            app.conf['pe_results_path'], preferred_event_id, 'bilby'
-        )
-
-        # For bilby_pipe PE ligo-py36 conda env needs to be sourced.
-        if app.conf['sentry_environment'] == 'production':
-            setup_arg = 'source /cvmfs/ligo-containers.opensciencegrid.org' +\
-                        '/lscsoft/conda/latest/etc/profile.d/conda.sh ' +\
-                        '&& conda activate ligo-py36 && bilby_pipe_gracedb ' +\
-                        '--webdir {} '.format(path_to_webdir) +\
-                        '--outdir {} --json {}'.format(rundir, path_to_json)
-
-        else:
-            setup_arg = 'source /cvmfs/ligo-containers.opensciencegrid.org' +\
-                        '/lscsoft/conda/latest/etc/profile.d/conda.sh ' +\
-                        '&& conda activate ligo-py36 && bilby_pipe_gracedb ' +\
-                        '--channel-dict o2replay --sampler-kwargs FastTest ' +\
-                        '--webdir {} '.format(path_to_webdir) +\
-                        '--outdir {} --json {}'.format(rundir, path_to_json)
-    try:
-        subprocess.run(
-            setup_arg, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE, check=True, shell=True
-        )
-        if pe_pipeline == 'bilby':
-
-            # Uploads bilby ini file to GraceDB
-            _upload_result(rundir, 'bilby_config.ini', superevent_id,
-                           'Automatically generated Bilby configuration file',
-                           'pe', 'online_bilby_pe.ini').delay()
-
-            path_to_dag = glob.glob(rundir + '/submit/dag*.submit')
-            dag_arg = 'condor_submit_dag -no_submit {}'.format(path_to_dag[0])
-
-        subprocess.run(
-            dag_arg, stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE, check=True, shell=True
-        )
-
-    except subprocess.CalledProcessError as e:
-        contents = b'args:\n' + json.dumps(e.args[1]).encode('utf-8') + \
-                   b'\n\nstdout:\n' + e.stdout + b'\n\nstderr:\n' + e.stderr
-        gracedb.upload.delay(
-            filecontents=contents, filename='pe_dag.log',
-            graceid=superevent_id,
-            message='Failed to prepare DAG', tags='pe'
-        )
-        shutil.rmtree(rundir)
+        canvas = ordered_group(
+            gracedb.download.si('coinc.xml', preferred_event_id),
+            _download_psd.si(preferred_event_id)
+        ) | _setup_dag_for_lalinference.s(ini_contents, rundir, superevent_id)
+    elif pe_pipeline == 'bilby':
+        canvas = gracedb.get_event.si(preferred_event_id) | \
+            _setup_dag_for_bilby(rundir, preferred_event_id, superevent_id)
+    else:
+        print("A PE pipeline named {} does not exist.".format(pe_pipeline))
         raise
-    finally:
-        # Remove the ini file so that people do not accidentally use this ini
-        # file and hence online-PE-only nodes.
-        if pe_pipeline == 'lalinference':
-            os.remove(path_to_ini)
-
-    if pe_pipeline == 'lalinference':
-        path_to_sub = rundir + '/multidag.dag.condor.sub'
-    if pe_pipeline == 'bilby':
-        sub = glob.glob(rundir + '/submit/dag*.submit.condor.sub')
-        path_to_sub = sub[0]
-    return path_to_sub
+    canvas |= _condor_no_submit.s()
+    return canvas
 
 
 def _find_paths_from_name(directory, name):
@@ -478,7 +519,6 @@ def dag_finished(rundir, preferred_event_id, superevent_id, pe_pipeline):
     tasks : canvas
         The work-flow for uploading PE results
     """
-
     if pe_pipeline == 'lalinference':
         # path to lalinference pe results
         pe_results_path = os.path.join(
@@ -567,9 +607,7 @@ def start_pe(ini_contents, preferred_event_id, superevent_id, pe_pipeline):
     pe_pipeline : str
         The parameter estimation pipeline used
         lalinference OR bilby
-
     """
-
     gracedb.upload.delay(
         filecontents=None, filename=None, graceid=superevent_id,
         message=('Starting {} online parameter estimation '
@@ -588,19 +626,8 @@ def start_pe(ini_contents, preferred_event_id, superevent_id, pe_pipeline):
     # ROTA people can check the status of parameter estimation.
     os.chmod(rundir, 0o755)
 
-    if pe_pipeline == 'lalinference':
-        canvas = ordered_group(
-            gracedb.download.s('coinc.xml', preferred_event_id),
-            _download_psd.s(preferred_event_id)
-        )
-
-    elif pe_pipeline == 'bilby':
-        canvas = gracedb.get_event.si(preferred_event_id)
-        # ini contents not passed to bilby
-        ini_contents = None
-
-    canvas |= (
-        dag_prepare.s(
+    canvas = (
+        dag_prepare_task(
             rundir, superevent_id, preferred_event_id,
             pe_pipeline, ini_contents
         )
