9b3fe216928cf67d032f77a41c05e727adceb428
Leo Singer <leo.singer@ligo.org>
2019-12-09 19:02:31 -0500

    Enable multiple mock uploads for test environment

--- gwcelery/conf/test.py
+++ gwcelery/conf/test.py
@@ -15,3 +15,7 @@ sentry_environment = 'test'
 """Record this `environment tag
 <https://docs.sentry.io/enriching-error-data/environments/>`)` in Sentry log
 messages."""
+
+mock_events_simulate_multiple_uploads = True
+"""If True, then upload each mock event several times in rapid succession with
+random jitter in order to simulate multiple pipeline uploads."""
