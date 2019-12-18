40428abf46ee416158f815a319f0d393401b80f8
Leo Singer <leo.singer@ligo.org>
2019-12-18 11:54:16 -0500

    Provide test requirements through setup.cfg

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_requirements(filename):
 
 setup_requires = ['setuptools >= 30.3.0']
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
-    setup_requires.extend(get_requirements('test-requirements.txt'))
+    setup_requires.append('pytest-runner')
 if {'build_sphinx'}.intersection(sys.argv):
     setup_requires.extend(get_requirements('docs-requirements.txt'))
 
