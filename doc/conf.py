9b4c6bc613802c332baba3b5a1e4a9a9969eaf7d
Leo Singer <leo.singer@ligo.org>
2021-02-24 23:02:28 -0500

    Fix another one

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ from pathlib import Path
 import pkg_resources
 import sys
 
-sys.path.insert(0, str(Path(__file__).parent))
+sys.path.insert(0, str(Path(__file__).parents[1]))
 
 
 # -- General configuration ------------------------------------------------
