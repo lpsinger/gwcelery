33b902580e7af79cff9580013acbdc760ef9efb1
Geoffrey Mo <geoffrey.mo@ligo.org>
2021-08-20 10:58:54 -0400

    Fix lint error

--- gwcelery/lvalert/__init__.py
+++ gwcelery/lvalert/__init__.py
@@ -9,6 +9,6 @@ from .bootsteps import Receiver
 def install(app):
     """Register the LVAlert subsystem in the application boot steps."""
     app.steps['consumer'] |= {Receiver}
-    app.user_options['worker'].add(Option(( '--lvalert',),
+    app.user_options['worker'].add(Option(('--lvalert',),
                                    is_flag=True,
                                    help='Enable LVAlert receiver'))
