37b03288d3484d5f5edc9e0193dacec7a482b84e
Leo Singer <leo.singer@ligo.org>
2022-04-04 13:11:58 -0400

    Fix Celery intersphinx URL

--- doc/conf.py
+++ doc/conf.py
@@ -208,7 +208,7 @@ intersphinx_mapping = {
     # FIXME: Import sphinx refs from the old version of Celery that we are
     # currently using. Change to 'latest' once we complete the upgrade to
     # Celery 5.
-    'celery': ('https://docs.celeryproject.org/en/latest/', None),
+    'celery': ('https://docs.celeryq.dev/en/stable/', None),
     'comet': ('https://comet.transientskp.org/en/stable/', None),
     'data-quality-report': (
         'https://docs.ligo.org/detchar/data-quality-report/', None),
